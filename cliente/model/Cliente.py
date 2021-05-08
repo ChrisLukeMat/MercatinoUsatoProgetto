fc0ee38852b91db24fbbca0bc1fdb9d8f96d836c
matty <s1095052@studenti.univpm.it>
2021-05-08 11:29:29 +0200

    modifica cliente

--- cliente/model/Cliente.py
+++ cliente/model/Cliente.py
@@ -1,5 +1,6 @@
 class Cliente:
-    def __init__(self, nome, cognome, cf):
+    def __init__(self, nome, cognome, cf, eta):
         self.nome = nome
         self.cognome = cognome
         self.cf = cf
+        self.eta = eta
