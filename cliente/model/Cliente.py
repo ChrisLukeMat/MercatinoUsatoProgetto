3ae00101316e536a4a76ef7b0c777969c6c6b2e1
matty <s1095052@studenti.univpm.it>
2021-05-15 12:44:47 +0200

    modifica VistaInserisciCliente e VistaListaClienti

--- cliente/model/Cliente.py
+++ cliente/model/Cliente.py
@@ -1,4 +1,9 @@
+import os
+import pickle
+
+
 class Cliente:
+    cnt = 0
     cod_cliente = 0
     def __init__(self, nome, cognome, cf, data_nascita, luogo_nascita, telefono, indirizzo):
         self.nome = nome
@@ -8,9 +13,12 @@ class Cliente:
         self.luogo_nascita = luogo_nascita
         self.telefono = telefono
         self.indirizzo = indirizzo
+        if Cliente.cnt == 0:
+            self.aggiorna_codice()
+            Cliente.cnt += 1
         self.id_cliente = self.cod_cliente + 1
         self.id_cliente = str(self.id_cliente) + "c"
-        self.cod_cliente += 1
+        Cliente.cod_cliente += 1
 
     def set_nome(self, nome):
         self.nome = nome
@@ -57,4 +65,11 @@ class Cliente:
     def get_id_cliente(self):
         return self.id_cliente
 
-
+    def aggiorna_codice(self):
+        if os.path.isfile('listaclienti/data/lista_clienti_salvata.pickle'):
+            with open('listaclienti/data/lista_clienti_salvata.pickle', 'rb') as f:
+                lista_clienti = pickle.load(f)
+                if len(lista_clienti) != 0:
+                    for cliente in lista_clienti:
+                        codice = cliente.get_id_cliente()
+                    Cliente.cod_cliente = int(str(codice).split('c')[0])
--- cliente/views/VistaCliente.py
+++ cliente/views/VistaCliente.py
@@ -1,3 +1,4 @@
+from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QHBoxLayout, QPushButton, QSizePolicy
 
 from cliente.controller.ControllerCliente import ControllerCliente
@@ -9,32 +10,46 @@ class VistaCliente(QWidget):
 
         h_layout = QHBoxLayout()
         v_layout = QVBoxLayout()
+        v_layout2 = QVBoxLayout()
 
-        v_layout.addWidget(self.get_generic_label(self.controller.get_nome()))
-        v_layout.addWidget(self.get_generic_label(self.controller.get_cognome()))
-        v_layout.addWidget(self.get_generic_label(self.controller.get_cf()))
-        v_layout.addWidget(self.get_generic_label(self.controller.get_data_nascita()))
-        v_layout.addWidget(self.get_generic_label(self.controller.get_luogo_nascita()))
-        v_layout.addWidget(self.get_generic_label(self.controller.get_indirizzo()))
-        v_layout.addWidget(self.get_generic_label(self.controller.get_telefono()))
-        v_layout.addWidget(self.get_generic_label(self.controller.get_id_cliente()))
+        v_layout.addWidget(self.get_generic_label("Nome: "))
+        v_layout.addWidget(self.get_generic_label("Cognome: "))
+        v_layout.addWidget(self.get_generic_label("Codice Fiscale: "))
+        v_layout.addWidget(self.get_generic_label("Data Nascita: "))
+        v_layout.addWidget(self.get_generic_label("Luogo Nascita: "))
+        v_layout.addWidget(self.get_generic_label("Indirizzo: "))
+        v_layout.addWidget(self.get_generic_label("Telefono: "))
+        v_layout.addWidget(self.get_generic_label("Id: "))
+
+        v_layout2.addWidget(self.get_generic_label(self.controller.get_nome()))
+        v_layout2.addWidget(self.get_generic_label(self.controller.get_cognome()))
+        v_layout2.addWidget(self.get_generic_label(self.controller.get_cf()))
+        v_layout2.addWidget(self.get_generic_label(str(self.controller.get_data_nascita())))
+        v_layout2.addWidget(self.get_generic_label(self.controller.get_luogo_nascita()))
+        v_layout2.addWidget(self.get_generic_label(self.controller.get_indirizzo()))
+        v_layout2.addWidget(self.get_generic_label(self.controller.get_telefono()))
+        v_layout2.addWidget(self.get_generic_label(self.controller.get_id_cliente()))
 
         h_layout.addLayout(v_layout)
+        h_layout.addLayout(v_layout2)
+        h_layout.setContentsMargins(15, 0, 125, 0)
+        h_layout.addStretch()
 
-        buttons_layout = QVBoxLayout()
-        #buttons_layout.addWidget(self.get_generic_button("Modifica", self.show_modifica_cliente))
+        #buttons_layout = QVBoxLayout()
         #buttons_layout.addWidget(self.get_generic_button("Elimina", self.show_elimina_cliente))
-        h_layout.addLayout(buttons_layout)
+        #h_layout.addLayout(buttons_layout)
 
         self.setLayout(h_layout)
         self.resize(500,400)
-        self.setWindowTitle("{} {}".format(cliente.get_nome, cliente.get_cognome()))
+        self.setWindowTitle("{} {}".format(cliente.get_nome(), cliente.get_cognome()))
 
     def get_generic_label(self, text):
-        label = QLabel(text)
-        font = label.font()
-        font.setPointSize(30)
-        label.setFont(font)
+        label = QLabel(str(text))
+        label.setFont(QFont('Arial', 10))
+        #font = label.font()
+        #font.setStyle(QFont())
+        #font.setPointSize(15)
+        #label.setFont(font)
         return label
 
     def get_generic_button(self, titolo, on_click):
@@ -43,4 +58,5 @@ class VistaCliente(QWidget):
         button.clicked.connect(on_click)
         return button
 
-    #def show_modifica_oggetto(self):
+    #def show_modifica_cliente(self):
+        #pass
