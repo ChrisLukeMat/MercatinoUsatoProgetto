9778803f0f23c03503a70bf379f78cd420c53bb9
LucaGuidi5 <s1094518@studenti.univpm.it>
2021-05-22 15:33:59 +0200

    Styles

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from home.views.VistaLogin import VistaLogin
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    qss = "appStyle/Darkeum.qss"
+    qss = "appStyle/Combinear.qss"
     with open(qss, "r") as fh:
         app.setStyleSheet(fh.read())
 
