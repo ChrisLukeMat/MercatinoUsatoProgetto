866b5a2ffe31b549ee607740484c93cc1c816485
chris <s1093726@studenti.univpm.it>
2021-06-25 12:11:36 +0200

    Grafici andamento incassi

--- incassi/VistaIncassi.py
+++ incassi/VistaIncassi.py
@@ -2,13 +2,12 @@ import json
 import os
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPushButton, QSizePolicy
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPushButton, QSizePolicy, QLabel
 from datetime import *
-from matplotlib import rcParams
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 
 class VistaIncassi(QWidget):
@@ -38,17 +37,20 @@ class VistaIncassi(QWidget):
 
     def show_settimana(self):
         self.riempi_liste(7)
-        self.grafico(10, 6)
-        '''prova = GraficoAndamento(self.lista_date, self.lista_incassi)
-        prova.show()'''
-
+        self.grafico(15, 9)
+        self.prova = GraficoAndamento()
+        self.prova.show()
 
     def show_mese(self):
         self.riempi_liste(30)
-        self.grafico(15, 8)
+        self.grafico(15, 9)
+        self.prova = GraficoAndamento()
+        self.prova.show()
 
     def riempi_liste(self, numero_giorni):
         lista_totale = []
+        self.lista_date.clear()
+        self.lista_incassi.clear()
         if os.path.isfile('incassi/incassi.json'):
             with open('incassi/incassi.json') as f:
                 lista_totale = json.load(f)
@@ -61,40 +63,38 @@ class VistaIncassi(QWidget):
                     self.lista_incassi.append(lista_totale[i].get("incasso"))
 
     def grafico(self, length, width):
-        rcParams['figure.figsize'] = length, width
+
+        myFmt = mdates.DateFormatter('%d/%m')
+        plt.gca().xaxis.set_major_formatter(myFmt)
 
         dates = matplotlib.dates.date2num(self.lista_date)
         matplotlib.pyplot.plot_date(dates, self.lista_incassi, marker='o', color='blue')
-        myFmt = mdates.DateFormatter('%d/%m')
-        plt.gca().xaxis.set_major_formatter(myFmt)
+        #plt.plot(range(len(self.lista_date)), self.lista_incassi, 'bo')
+        #plt.xticks(range(len(self.lista_date)), dates)
+
         plt.xticks(dates)
-        plt.yticks(self.lista_incassi)
+        ax = plt.gca()
+        #plt.yticks(self.lista_incassi)
+        for i,v in enumerate(self.lista_incassi):
+            ax.annotate(str(v), xy=(i,v), xytext=(-7, 7), textcoords='offset points')
         plt.xlabel("Giorni")
+        plt.ylabel("Incassi (€)")
         plt.gcf().autofmt_xdate()
-        plt.show()
+        plt.gcf().set_size_inches(length, width)
+        plt.savefig('incassi/graficoIncassi.svg', format='svg', dpi=1200)
 
 
-'''class GraficoAndamento(QWidget):
-    def __init__(self,lista_date, lista_incassi, parent = None):
+class GraficoAndamento(QWidget):
+    def __init__(self, parent = None):
         super(GraficoAndamento, self).__init__(parent)
 
-        self.lista_date = lista_date
-        self.lista_incassi = lista_incassi
-        v_layout = QVBoxLayout()
-        self.figure = plt.figure()
-        self.canvas = FigureCanvas(self.figure)
-        v_layout.addWidget(self.canvas)
-        self.grafico(10, 6)
+        self.pixmap = QPixmap('incassi/graficoIncassi.svg')
+        self.label = QLabel()
+        self.label.setPixmap(self.pixmap)
+        lay = QVBoxLayout()
+        lay.addWidget(self.label)
+        lay.setAlignment(Qt.AlignCenter)
 
-    def grafico(self, length, width):
-        rcParams['figure.figsize'] = length, width
-        graph = self.canvas.figure.subplots()
-        dates = matplotlib.dates.date2num(self.lista_date)
-        graph.matplotlib.pyplot.plot_date(dates, self.lista_incassi, marker='o', color='blue')
-        myFmt = mdates.DateFormatter('%d/%m')
-        plt.gca().xaxis.set_major_formatter(myFmt)
-        plt.xticks(dates)
-        plt.yticks(self.lista_incassi)
-        plt.xlabel("Giorni")
-        plt.gcf().autofmt_xdate()
-        self.canvas.draw()'''
+        self.setLayout(lay)
+
+        self.setWindowTitle("Grafico andamento incassi")
